f11fc6806e68301baa96d748033cc206b7aa0fe7
kanogame <wasdqert1222@gmail.com>
2023-04-06 16:56:13 +0700

    go post finished

--- DiscordApp - webserver/HttpListenerClient/HttpFunction.go
+++ DiscordApp - webserver/HttpListenerClient/HttpFunction.go
@@ -2,9 +2,12 @@ package httplistenerclient
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
+	databaseHandler "main/DatabaseHandler"
 	utils "main/Utils"
 	"net/http"
+	"net/url"
 )
 
 func postPlayerPause(w http.ResponseWriter, r *http.Request) {
@@ -17,5 +20,18 @@ func postPlayerPause(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		panic(err)
 	}
+	var getGuild utils.GetGuildJSON
+	getGuild.Token = post.Token
+	getGuild.Password = post.Password
+	db := databaseHandler.CreateNewConnection()
+	guildid := databaseHandler.GetGuildId(db, getGuild)
 
+	data := url.Values{
+		"guildid": {guildid},
+	}
+
+	resp, err := http.PostForm("http://localhost:13532/player/pause", data)
+	var success map[string]interface{}
+	json.NewDecoder(resp.Body).Decode(&success)
+	fmt.Println(success)
 }
