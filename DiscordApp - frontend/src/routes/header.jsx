06149c520769ea1f28b43e387ac48d72c6eb62c0
kanogame <wasdqert1222@gmail.com>
2023-04-13 18:05:18 +0700

    some minor stuff

--- DiscordApp - frontend/src/routes/header.jsx
+++ DiscordApp - frontend/src/routes/header.jsx
@@ -2,39 +2,43 @@ import styled from "styled-components";
 
 export default function Header() {
     const Root = styled.div`
-    height: 100vh;`
-
-    const HeaderContent = styled.div`
-        width: 100%;
-        max-width: 1800;
-        background: rgb(250,255,73);
-        background: linear-gradient(90deg, rgba(250,255,73,1) 0%, rgba(255,196,85,1) 100%);`
+    height: 100vh;`;
 
     const Header = styled.div`
-    margin: 0px 100px;
-    height: 50px;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;`
+        width: 100%;
+        background: rgb(250,255,73);
+        background: linear-gradient(90deg, rgba(250,255,73,1) 0%, rgba(255,196,85,1) 100%);
+        display: flex;`;
+
+    const HeaderContent = styled.div`
+        height: 50px;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;`;
 
     const Tabs = styled.div`
-    display: flex;
-    gap: 50px;`
+        display: flex;
+        gap: 50px;`;
 
     const Tab = styled.div`
-    font-size: 20px;`
+        font-size: 20px;`;
+
+    const Account = styled.div`
+        height: 30px;
+        width: 40px;`;
 
     return (
         <Root>
-            <HeaderContent>
-                <Header>
+            <Header>
+                <HeaderContent>
                     <Tabs>
-                        <Tab>TEST1</Tab>
-                        <Tab>TEST1</Tab>
-                        <Tab>TEST1</Tab>
+                        <Tab>Главная</Tab>
+                        <Tab>Мои сервера</Tab>
+                        <Tab>Плеер</Tab>
                     </Tabs>
-                </Header>
-            </HeaderContent>
+                    <Account></Account>
+                </HeaderContent>
+            </Header>
         </Root>
     )
 }
