f3b138b8720a347b1df451e6d54b08caa966d510
kanogame <wasdqert1222@gmail.com>
2023-04-20 19:13:51 +0700

    front fixes 4.5.5

--- DiscordApp - frontend/src/routes/player.jsx
+++ DiscordApp - frontend/src/routes/player.jsx
@@ -2,6 +2,56 @@ import { useLoaderData } from "react-router-dom";
 import { useState } from "react";
 import styled from "styled-components";
 
+const ModalContent = styled.div`
+    display: flex;
+    width: 100%;
+    height: 100%;  
+    justify-content: center;
+    align-items: center;
+    background: rgba(0, 0, 0, 0.5);`;
+
+const Modal = styled.div`
+    padding: 30px;
+    background: white;
+    border: 1px solid rgb(179, 179, 179);
+    border-radius: 16px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 15px;`;
+
+const ModalHead = styled.div`
+    font-size: 20px;
+    text-align: center;
+    font-weight: 500;`;
+
+const ModalInput = styled.input`
+    font-size: 1rem;
+    font-family: inherit;
+    border: none;
+    border-radius: 8px;
+    padding: 0.5rem 0.75rem;
+    box-shadow: 0 0px 1px hsla(0, 0%, 0%, 0.2), 0 1px 2px hsla(0, 0%, 0%, 0.2);
+    background-color: white;
+    line-height: 1.5;
+    margin: 0;
+    &:hover {
+        box-shadow: 0 0px 1px hsla(0, 0%, 0%, 0.6), 0 1px 2px hsla(0, 0%, 0%, 0.2);
+    }`;
+
+const ModalButton = styled.a`
+    text-decoration: none;
+    color: black;
+    padding: 5px;
+    background: rgb(250,255,73);  
+    border: 3px solid #c6cc21;
+    border-radius: 5px;
+    cursor: pointer;
+    &:hover {
+        background: #bbc020;
+    }
+`;
+
 export function loader({params}) {
     console.log(params.tokenPass);
     const tokenPass = params.tokenPass;
@@ -13,59 +63,8 @@ export default function PlayerSection() {
     const {tokenPass} = useLoaderData();
 
     function setTokenInp(e) {
-        setToken(e.target.value)
+        setToken(e.target.value);
     }
-
-    const ModalContent = styled.div`
-        display: flex;
-        width: 100%;
-        height: 100%;  
-        justify-content: center;
-        align-items: center;
-        background: rgba(0, 0, 0, 0.5);
-        display: none;`;
-
-    const Modal = styled.div`
-        padding: 30px;
-        background: white;
-        border: 1px solid rgb(179, 179, 179);
-        border-radius: 16px;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        gap: 15px;`;
-
-    const ModalHead = styled.div`
-        font-size: 20px;
-        text-align: center;
-        font-weight: 500;`;
-
-    const ModalInput = styled.input`
-        font-size: 1rem;
-        font-family: inherit;
-        border: none;
-        border-radius: 8px;
-        padding: 0.5rem 0.75rem;
-        box-shadow: 0 0px 1px hsla(0, 0%, 0%, 0.2), 0 1px 2px hsla(0, 0%, 0%, 0.2);
-        background-color: white;
-        line-height: 1.5;
-        margin: 0;
-        &:hover {
-            box-shadow: 0 0px 1px hsla(0, 0%, 0%, 0.6), 0 1px 2px hsla(0, 0%, 0%, 0.2);
-        }`;
-    
-    const ModalButton = styled.a`
-        text-decoration: none;
-        color: black;
-        padding: 5px;
-        background: rgb(250,255,73);  
-        border: 3px solid #c6cc21;
-        border-radius: 5px;
-        cursor: pointer;
-        &:hover {
-            background: #bbc020;
-        }
-    `;
 
     return <>
         <ModalContent>
@@ -75,9 +74,7 @@ export default function PlayerSection() {
                 <ModalButton href={`/player/${tokenPass}&t=${token}`}>Готово</ModalButton>
             </Modal>
         </ModalContent>
-        <Player musiclist={[{id: 1, title: "test music", length: "15:32", url: "https://youtube.com", request: "Kanogames"}]}>
-
-        </Player>
+        
     </>
 }
 
@@ -115,5 +112,5 @@ function Player(props) {
 }
 
 function Music(props) {
-    
+
 }
--- DiscordApp - frontend/src/routes/root.jsx
+++ DiscordApp - frontend/src/routes/root.jsx
@@ -3,43 +3,43 @@ import React from 'react';
 import {ReactComponent as Logo} from '../images/logo.svg';
 import { Outlet } from "react-router-dom";
 
+const RootDiv = styled.div`
+height: 100vh;`;
+
+const Header = styled.div`
+    width: 100%;
+    padding: 0 30px;
+    background: rgb(250,255,73);
+    background: linear-gradient(90deg, rgba(250,255,73,1) 0%, rgba(255,196,85,1) 100%);
+    font-family: Raleway;
+    display: flex;
+    border-bottom: 1px solid lightgray;`;
+
+const HeaderContent = styled.div`
+    height: 50px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;`;
+
+const Tabs = styled.div`
+    align-items: center;
+    display: flex;
+    gap: 40px;`;
+
+const Tab = styled.div`
+    font-size: 16px;
+    font-weight: 600;`;
+
+const Account = styled.div`
+    height: 30px;
+    width: 40px;`;
+
+const Main = styled.div`
+    height: calc(100vh - 100px);`;
+
 export default function Root() {
-    const Root = styled.div`
-    height: 100vh;`;
-
-    const Header = styled.div`
-        width: 100%;
-        padding: 0 30px;
-        background: rgb(250,255,73);
-        background: linear-gradient(90deg, rgba(250,255,73,1) 0%, rgba(255,196,85,1) 100%);
-        font-family: Raleway;
-        display: flex;
-        border-bottom: 1px solid lightgray;`;
-
-    const HeaderContent = styled.div`
-        height: 50px;
-        display: flex;
-        justify-content: space-between;
-        align-items: center;`;
-
-    const Tabs = styled.div`
-        align-items: center;
-        display: flex;
-        gap: 40px;`;
-
-    const Tab = styled.div`
-        font-size: 16px;
-        font-weight: 600;`;
-
-    const Account = styled.div`
-        height: 30px;
-        width: 40px;`;
-
-    const Main = styled.div`
-        height: calc(100vh - 100px);`;
-
     return (
-        <Root>
+        <RootDiv>
             <Header>
                 <HeaderContent>
                     <Tabs>
@@ -52,6 +52,6 @@ export default function Root() {
                 </HeaderContent>
             </Header>
             <Main><Outlet /></Main>
-        </Root>
+        </RootDiv>
     )
 }
