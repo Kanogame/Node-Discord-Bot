f4725c1cb1d6f32badcfdd1c745b1e240bbc2328
kanogame <wasdqert1222@gmail.com>
2023-05-01 20:30:32 +0700

    music moved

---
+++ DiscordApp - frontend/src/components/music.jsx
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+
+export default function Music(props) {
+    const MusicEl = styled.div`
+        padding: 10px;
+        color: black;
+        border: 1px solid lightgray;`;
+
+    const MusicHead = styled.div`
+        display: flex;
+        justify-content: space-between;`;
+
+    const MusicTitle = styled.div`
+        font-size: 20px;`;
+
+    const MusicTime = styled.div`
+        font-size: 14px;`;
+
+    const MusicType = styled.div`
+        font-size: 14px;`;    
+
+    const MusicUrl = styled.div`
+        font-size: 14px;
+        display: inline-block;
+        margin-left: 10px;
+        color: lightgray;`;  
+
+    return <MusicEl>
+        <MusicHead>
+            <MusicTitle>{props.title}</MusicTitle>
+            <MusicTime>{props.length}</MusicTime>
+        </MusicHead>
+        <MusicHead>
+            <MusicType>Youtube<MusicUrl>{props.url}</MusicUrl></MusicType>
+            <MusicUrl>{"reqested by:" + props.request}</MusicUrl>
+        </MusicHead>
+    </MusicEl>
+}
