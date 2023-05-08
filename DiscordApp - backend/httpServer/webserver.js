c3db6cc5bf568f85cddb6e17a54753c3bd11e6ef
kanogame <wasdqert1222@gmail.com>
2023-05-08 11:57:12 +0700

    websokets pt1

--- DiscordApp - backend/httpServer/webserver.js
+++ DiscordApp - backend/httpServer/webserver.js
@@ -2,10 +2,36 @@ const WebSocket = require("ws");
 
 module.exports = { startWSserver }
 
-function startWSserver() {
-    const WsServer = new WebSocket.Server({ port: 9000 });
+class WebSocketServer {
+    constructor(port) {
+        this.WsServer = new WebSocket.Server({ port: port });
+        this.Nextid = 0;
+        this.Clients = new Map();
+        setServer();
+    }
 
-    WsServer.on("connection", () => { console.log("connected") });
+    startServer() {
+        this.WsServer.on("connection", onConnect);
+    }
+
+    onConnect(wsClient) {  
+        wsClient.on("message", onMessage);
+    }
+
+    onMessage(messageStr) {
+        const message = JSON.parse(messageStr);
+        console.log(message);
+    }
+}
+
+function startWSserver() {
+    
+
+    WsServer.on("connection", onConnect);
     
     WsServer.on("close", () => { console.log("closed") });
+}
+
+function onConnect() {
+
 }
